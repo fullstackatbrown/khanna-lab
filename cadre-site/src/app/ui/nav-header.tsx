06a3b6d2ba4ee1f1c0e6e78e405d0b37c01f80b8
myang1220 <myang1220@gmail.com>
2024-11-06 15:18:04 -0500

    feat: add image and modify header color and text

--- cadre-site/src/app/ui/nav-header.tsx
+++ cadre-site/src/app/ui/nav-header.tsx
@@ -1,52 +1,75 @@
 'use client';
 
 import React, { useState, useEffect } from 'react';
-import ReactDOM from "react-dom";
+import ReactDOM from 'react-dom';
 import Image from 'next/image';
 import Link from 'next/link';
 
-
 export default function NavHeader() {
-    const [position, setPosition] = useState(0);
-    const [visible, setVisible] = useState(true);
+  const [position, setPosition] = useState(0);
+  const [visible, setVisible] = useState(true);
 
-    useEffect(() => {
-        setPosition(window.scrollY);
+  useEffect(() => {
+    setPosition(window.scrollY);
 
-        const handleScroll = () => {
-            let moving = window.scrollY;
-            setVisible(position > moving);
-            setPosition(moving);
-        };
-        window.addEventListener("scroll", handleScroll);
-        return () => {
-            window.removeEventListener("scroll", handleScroll);
-        };
-    }, [position]);
+    const handleScroll = () => {
+      let moving = window.scrollY;
+      setVisible(position > moving);
+      setPosition(moving);
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [position]);
 
-    const headerClass = visible ? 'nav-header' : 'nav-header nav-header-hidden';
+  const headerClass = visible ? 'nav-header' : 'nav-header nav-header-hidden';
 
-    return (
-        <div id="header" className={`fixed flex flex-row justify-between bg-primary items-center w-full min-h-[16] pl-3 pr-7 z-[100]  ${headerClass}`} style={{ borderBottom: '3px solid rgb(255,255,255)'}}>
-            {/* <Link href="/">
-                <Image
-                    src="/images/logo.png"
-                    width={80}
-                    height={20}
-                    alt="logo"
-                    style={{ cursor: 'pointer' }}
-                />
-            </Link> */}
-            <Link className="fade-in-out-basic hover:text-primary-gold text-[rgb(250,250,250)] min-w-[185px]" href="localhost">
-                <div className="font-mp font-semibold gap-5 px-5 text-4xl-responsive pt-0.5">
-                    Lorem Ipsum
-                </div>
-            </Link>
-            <div className="font-circ-std flex flex-col md6:flex-row items-center justify-center md6:gap-6 gap-2 my-4 px-0">
-                <a href="about" className="text-[rgb(250,250,250)] hover:text-primary-gray flex-none text-lg fade-in-out-basic">About/Collaborators</a>
-                <a href="methods" className="text-[rgb(250,250,250)] hover:text-primary-gray flex-none text-lg fade-in-out-basic">Documentation</a>
-                <a href="download" className="text-[rgb(250,250,250)] hover:text-primary-gray flex-none text-lg fade-in-out-basic">Download</a>
-            </div>
+  return (
+    <div
+      id="header"
+      className={`fixed z-[100] flex min-h-[16] w-full flex-row items-center justify-between bg-white pl-3 pr-7  ${headerClass}`}
+      style={{ borderBottom: '3px solid rgb(255,255,255)' }}
+    >
+      <Link
+        className="fade-in-out-basic min-w-[185px] text-[rgb(250,250,250)] hover:text-primary-gold"
+        href="/"
+      >
+        <Image
+          src="/brownHealthLogo.png"
+          alt="Brown School of Public Health Logo"
+          width={240}
+          height={90}
+          className="cursor-pointer p-4"
+        />
+      </Link>
+      <div>
+        <div className="my-4 text-center text-3xl text-black">
+          <div className="text-base sm:text-base md:text-2xl lg:text-3xl">
+            MediCOIN (Medicare COding INtensity) Report
+          </div>
         </div>
-    )
+        <div className="my-4 flex flex-col items-center justify-end gap-2 px-0 font-circ-std md6:flex-row md6:gap-16">
+          <a
+            href="methods"
+            className="fade-in-out-basic flex-none text-base text-[rgb(0,0,0)] hover:text-primary-gray"
+          >
+            Methods
+          </a>
+          <a
+            href="download"
+            className="fade-in-out-basic flex-none text-base text-[rgb(0,0,0)] hover:text-primary-gray"
+          >
+            Download
+          </a>
+          <a
+            href="view"
+            className="fade-in-out-basic flex-none text-base text-[rgb(0,0,0)] hover:text-primary-gray"
+          >
+            View Data & Findings
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }

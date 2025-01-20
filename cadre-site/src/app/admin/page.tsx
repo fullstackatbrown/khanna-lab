20e830baadef2c0be3e629830039b8c9220ad3dc
ryane008 <ryan_eng@brown.edu>
2025-01-19 22:17:00 -0500

    test

--- cadre-site/src/app/admin/page.tsx
+++ cadre-site/src/app/admin/page.tsx
@@ -32,84 +32,84 @@ import {firestore, auth} from '../firebaseConfig'
  * @returns firebase auth ui component
  */
 export default function Page() {
-  // const { isAdmin, setAdmin } = useGlobalState();
+//   // const { isAdmin, setAdmin } = useGlobalState();
 
-  // var isAdmin = useRecoilValue(admin);
-  // var setAdmin = useSetRecoilState(admin);
-  const [isAdmin, setAdmin] = useRecoilState(adminState);
-  const [currentUser, setCurrentUser] = useRecoilState(current);
-  const [name, setName] = useState<string>('');
+//   // var isAdmin = useRecoilValue(admin);
+//   // var setAdmin = useSetRecoilState(admin);
+//   const [isAdmin, setAdmin] = useRecoilState(adminState);
+//   const [currentUser, setCurrentUser] = useRecoilState(current);
+//   const [name, setName] = useState<string>('');
 
-  const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_LOGIN_API_KEY,
-    authDomain: 'meyers-lab.firebaseapp.com',
-    projectId: 'meyers-lab',
-    storageBucket: 'meyers-lab.appspot.com',
-    messagingSenderId: process.env.NEXT_PUBLIC_APP_MSG,
-    appId: process.env.NEXT_PUBLIC_APP_APP,
-    measurementId: process.env.NEXT_PUBLIC_APP_MSR,
-  };
+//   const firebaseConfig = {
+//     apiKey: process.env.NEXT_PUBLIC_LOGIN_API_KEY,
+//     authDomain: 'meyers-lab.firebaseapp.com',
+//     projectId: 'meyers-lab',
+//     storageBucket: 'meyers-lab.appspot.com',
+//     messagingSenderId: process.env.NEXT_PUBLIC_APP_MSG,
+//     appId: process.env.NEXT_PUBLIC_APP_APP,
+//     measurementId: process.env.NEXT_PUBLIC_APP_MSR,
+//   };
 
-    useEffect(() => {
-      console.log('isAdmin in Admin:', isAdmin);
-    }, [isAdmin]);
+//     useEffect(() => {
+//       console.log('isAdmin in Admin:', isAdmin);
+//     }, [isAdmin]);
 
-   if (!getApps().length) {
-     initializeApp(firebaseConfig);
-   }
+//    if (!getApps().length) {
+//      initializeApp(firebaseConfig);
+//    }
 
-  const firestore = getFirestore();
+//   const firestore = getFirestore();
 
-  const Auth = () => {
-    return <AuthEmail></AuthEmail>;
-  };
+//   const Auth = () => {
+//     return <AuthEmail></AuthEmail>;
+//   };
 
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      auth.onAuthStateChanged((user) => {
-        if (user) {
+//   useEffect(() => {
+//     if (typeof window !== 'undefined') {
+//       auth.onAuthStateChanged((user) => {
+//         if (user) {
 
-          const userId = user.uid;
-          const email = user.email;
-          const name = user.displayName;
-          if (name) {
-            setName(name);
-          }
-          if (email) {
-            const document = doc(firestore, 'users', email);
-            getDoc(document).then((gotDoc) => {
-              if (gotDoc.exists()) {
-                setAdmin(gotDoc.data().admin);
-                setCurrentUser(email);
-              }
-            });
-          }
-        } else {
-          console.log('Authentication error:', user);
-        }
-      });
-    }
-  }, []);
+//           const userId = user.uid;
+//           const email = user.email;
+//           const name = user.displayName;
+//           if (name) {
+//             setName(name);
+//           }
+//           if (email) {
+//             const document = doc(firestore, 'users', email);
+//             getDoc(document).then((gotDoc) => {
+//               if (gotDoc.exists()) {
+//                 setAdmin(gotDoc.data().admin);
+//                 setCurrentUser(email);
+//               }
+//             });
+//           }
+//         } else {
+//           console.log('Authentication error:', user);
+//         }
+//       });
+//     }
+//   }, []);
 
-  const [headerHeight, setHeaderHeight] = useState(0);
+//   const [headerHeight, setHeaderHeight] = useState(0);
 
-  useEffect(() => {
-    const updateHeaderHeight = () => {
-      const header = document.getElementById('header');
-      if (header) {
-        setHeaderHeight(header.offsetHeight + 50);
-      }
-    };
-    if (typeof window !== 'undefined') {
-      updateHeaderHeight();
-      window.addEventListener('resize', updateHeaderHeight);
-    }
-    return () => {
-      if (typeof window !== 'undefined') {
-        window.removeEventListener('resize', updateHeaderHeight);
-      }
-    };
-  }, []);
+//   useEffect(() => {
+//     const updateHeaderHeight = () => {
+//       const header = document.getElementById('header');
+//       if (header) {
+//         setHeaderHeight(header.offsetHeight + 50);
+//       }
+//     };
+//     if (typeof window !== 'undefined') {
+//       updateHeaderHeight();
+//       window.addEventListener('resize', updateHeaderHeight);
+//     }
+//     return () => {
+//       if (typeof window !== 'undefined') {
+//         window.removeEventListener('resize', updateHeaderHeight);
+//       }
+//     };
+//   }, []);
 
   return (
     <div></div>
